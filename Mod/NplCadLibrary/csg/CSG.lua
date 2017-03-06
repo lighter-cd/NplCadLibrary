2b9c7b797ff1632aa47cf6c38c8c32e0826f7183
skeleton <121157@qq.com>
2017-03-07 01:39:59 +0800

    remove log

--- Mod/NplCadLibrary/csg/CSG.lua
+++ Mod/NplCadLibrary/csg/CSG.lua
@@ -138,42 +138,18 @@ function CSG:subtract(csg)
 	local aa = self:clone();
 	local bb = csg:clone();
 	LOG.std(nil, "info", "CSG:subtract", "vertex length of aa:%d,bb:%d", aa:getVertexCnt(), bb:getVertexCnt());
-	local fromTime = ParaGlobal.timeGetTime();
 	local a = CSGBSPNode:new():init(aa.polygons,aa.convex);
-	LOG.std(nil, "info", "a = CSG:subtract", "a = CSGBSPNode:new():init in %.3f seconds", (ParaGlobal.timeGetTime()-fromTime)/1000);
-	fromTime = ParaGlobal.timeGetTime();
 	local b = CSGBSPNode:new():init(bb.polygons,bb.convex);
-	LOG.std(nil, "info", "a = CSG:subtract", "b = CSGBSPNode:new():init in %.3f seconds", (ParaGlobal.timeGetTime()-fromTime)/1000);
 	LOG.std(nil, "info", "CSG:subtract", "vertex length of a:%d,b:%d", a:getVertexCnt(), b:getVertexCnt());
-
-	fromTime = ParaGlobal.timeGetTime();
 	a:invert();
-	LOG.std(nil, "info", "a = CSG:subtract", "a:invert() in %.3f seconds", (ParaGlobal.timeGetTime()-fromTime)/1000);
-	fromTime = ParaGlobal.timeGetTime();
     a:clipTo(b);
-	LOG.std(nil, "info", "a = CSG:subtract", "a:clipTo(b) in %.3f seconds", (ParaGlobal.timeGetTime()-fromTime)/1000);
-	fromTime = ParaGlobal.timeGetTime();
     b:clipTo(a);
-	LOG.std(nil, "info", "a = CSG:subtract", "b:clipTo(a) in %.3f seconds", (ParaGlobal.timeGetTime()-fromTime)/1000);
-	fromTime = ParaGlobal.timeGetTime();
     b:invert();
-	LOG.std(nil, "info", "a = CSG:subtract", "b:invert() in %.3f seconds", (ParaGlobal.timeGetTime()-fromTime)/1000);
-	fromTime = ParaGlobal.timeGetTime();
     b:clipTo(a);
-	LOG.std(nil, "info", "a = CSG:subtract", "b:clipTo(a) in %.3f seconds", (ParaGlobal.timeGetTime()-fromTime)/1000);
-	fromTime = ParaGlobal.timeGetTime();
     b:invert();
-	LOG.std(nil, "info", "a = CSG:subtract", "b:invert() in %.3f seconds", (ParaGlobal.timeGetTime()-fromTime)/1000);
-	fromTime = ParaGlobal.timeGetTime();
     a:build(b:allPolygons());
-	LOG.std(nil, "info", "a = CSG:subtract", "a:build(b:allPolygons()) in %.3f seconds", (ParaGlobal.timeGetTime()-fromTime)/1000);
-	fromTime = ParaGlobal.timeGetTime();
     a:invert();
-	LOG.std(nil, "info", "a = CSG:subtract", "a:invert() in %.3f seconds", (ParaGlobal.timeGetTime()-fromTime)/1000);
-	fromTime = ParaGlobal.timeGetTime();
-	local csg_result = CSG.fromPolygons(a:allPolygons());
-	LOG.std(nil, "info", "a = CSG:subtract", "CSG.fromPolygons(a:allPolygons()) in %.3f seconds", (ParaGlobal.timeGetTime()-fromTime)/1000);
-    return csg_result;
+    return CSG.fromPolygons(a:allPolygons());
 end
 -- Return a new CSG solid representing space both this solid and in the
 -- solid `csg`. Neither this solid nor the solid `csg` are modified.
